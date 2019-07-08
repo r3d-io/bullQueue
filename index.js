9063bacea833951f5e97b4b88d995afb304569d7
Anuj singh <askoli@techracers.io>
2019-07-08 17:01:14 +0530

    Updated queue closing procedure

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ let testnet = `https://rinkeby.infura.io/${process.env.INFURA_ACCESS_TOKEN}`
 let web3 = new Web3(new Web3.providers.HttpProvider(testnet))
 
 async function getBlockDetail(block_address) {
-  let blockDetail = await web3.eth.getBlock(block_address)
-  return blockDetail.transactions
+  try {
+    let blockDetail = await web3.eth.getBlock(block_address)
+    return blockDetail.transactions
+  }
+  catch (error) {
+    console.log(`unable to get block details ${error}`)
+  }
 }
 
 function addQueue(transactions, transactionQueue) {
@@ -30,8 +35,10 @@ async function processTransaction(block_address, transArray, transactionQueue) {
       console.log(`proccessed transaction ${trans} on queue ${block_address}`)
       jobDone();
       // console.log(`length ${transArray.length} === ${transactionCount}`)
-      if (transArray.length === transactionCount)
+      if (transArray.length === transactionCount) {
         writeToFile(block_address, transArray)
+        transactionQueue.close()
+      }
     });
   }
   catch (error) {
@@ -40,18 +47,21 @@ async function processTransaction(block_address, transArray, transactionQueue) {
 }
 
 async function queueCompletion(transArray, transactionQueue) {
-  transactionQueue.on('global: completed', (job, result) => {
+  transactionQueue.on('completed', (job, result) => {
     job.remove();
-    transactionQueue.close()
-    console.log(transArray)
-    console.log(`Job with id ${result} has been completed`);
+    // console.log(job)
+    console.log(`Job with id ${job.id} has been completed`);
   })
 }
 
 function writeToFile(block_address, transArray) {
+  var dir = './block-transaction';
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
   var json = JSON.stringify(transArray);
   try {
-    fs.writeFile(`${block_address}.json`, json, 'utf8', function (err) {
+    fs.writeFile(`${dir}/${block_address}.json`, json, 'utf8', function (err) {
       if (err) throw err;
       console.log(`completed file write for ${block_address}`);
       // process.exit()
@@ -62,14 +72,9 @@ function writeToFile(block_address, transArray) {
   }
 }
 
-async function main() {
-  // let block_address = process.argv[2] || "0xcb7695375b0948528396db9a4377923b857de1823273585fa70fca3e7fc8b19d"
-  let block_arr = ["0xd6138375419d0d82e333b2e84998f88eb42563dcf46cbfa19eefbecdcd5935e7",
-    "0xcb7695375b0948528396db9a4377923b857de1823273585fa70fca3e7fc8b19d",
-    "0x8e4ec07e3f9de5faf12fa9e25afe86a9830dbea20d95c18b7a7d7c951619f7e5"
-  ]
+async function main(block_arr) {
   let transactionQueue = {}
-  let transArray = {} 
+  let transArray = {}
   block_arr.forEach(async address => {
     transArray[address] = []
     transactionQueue[address] = new Bull(address);
@@ -80,4 +85,8 @@ async function main() {
   });
 }
 
-main()
+let block_arr = ["0xd6138375419d0d82e333b2e84998f88eb42563dcf46cbfa19eefbecdcd5935e7",
+  "0xcb7695375b0948528396db9a4377923b857de1823273585fa70fca3e7fc8b19d",
+  "0x8e4ec07e3f9de5faf12fa9e25afe86a9830dbea20d95c18b7a7d7c951619f7e5"
+]
+main(block_arr)
